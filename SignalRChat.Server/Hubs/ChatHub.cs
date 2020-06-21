9af4343bdf60da6257d9b905bc8fd4faf4c972b6
Minh Tran <trancongminh503@gmail.com>
2020-06-21 19:57:37 +0700

    Add Register method

--- SignalRChat.Server/Hubs/ChatHub.cs
+++ SignalRChat.Server/Hubs/ChatHub.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using SignalRChat.Shared;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SignalRChat.Server.Hubs
 {
 	public class ChatHub : Hub
 	{
-		public async Task SendMessage(string user, string message)
+      private static readonly Dictionary<string, string> userLookup = 
+         new Dictionary<string, string>();
+
+      public async Task SendMessage(string user, string message)
 		{
 			await Clients.Others.SendAsync(HubConstant.ReceiveMessageMethod, user, message);
 		}
-	}
+
+      public async Task Register(string username)
+      {
+         var currentId = Context.ConnectionId;
+         if (!userLookup.ContainsKey(currentId))
+         {
+            userLookup.Add(currentId, username);
+            await Clients.Others.SendAsync(HubConstant.RegisterMethod, username);
+         }
+      }
+   }
 }
