c2e00dcc64709b648224a54c9f574251f90d6a7d
Minh Tran <trancongminh503@gmail.com>
2020-06-24 22:09:13 +0700

    BasicChatScreen

--- SignalRChat.Clients.Blazor/Pages/Chat.razor.cs
+++ SignalRChat.Clients.Blazor/Pages/Chat.razor.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SignalRChat.Shared;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,37 +8,41 @@ namespace SignalRChat.Clients.Blazor.Pages
 {
 	public class ChatBase : ComponentBase
 	{
-		private ChatClient chatClient;
+		protected ChatClient chatClient;
 		protected List<MessageReceivedEventArgs> chatMessages;
 		protected string myChatMessage = string.Empty;
-
-		protected override async Task OnInitializedAsync()
-		{
-			chatMessages = new List<MessageReceivedEventArgs>();
-			chatClient = new ChatClient($"{DateTime.Now}", "http://localhost:5000");
-			chatClient.MessageReceived += ChatClient_MessageReceived;
-			chatClient.NotificationStateChange += ChatClient_NotificationStateChange;
-			await chatClient.StartAsync();
-		}
+		protected string username = string.Empty;
 
 		private void ChatClient_NotificationStateChange(object sender, NotifyStateEventArgs e)
 		{
 			chatMessages.Add(new MessageReceivedEventArgs()
 			{
-				UserName = e.Username,
+				ClientIdentity = e.ClientIdentity,
 				Message = e.State.ToString()
 			});
+			StateHasChanged();
 		}
 
 		private void ChatClient_MessageReceived(object sender, MessageReceivedEventArgs e)
 		{
 			chatMessages.Add(e);
+			StateHasChanged();
 		}
 
 		protected async Task HandleSendMessage(MouseEventArgs args)
 		{
 			await chatClient.SendAsync(myChatMessage);
 			myChatMessage = string.Empty;
+			StateHasChanged();
+		}
+
+		protected async Task RegisUser()
+		{
+			chatMessages = new List<MessageReceivedEventArgs>();
+			chatClient = new ChatClient($"{username}", "http://localhost:5000");
+			chatClient.MessageReceived += ChatClient_MessageReceived;
+			chatClient.NotificationStateChange += ChatClient_NotificationStateChange;
+			await chatClient.StartAsync();
 		}
 	}
 }
