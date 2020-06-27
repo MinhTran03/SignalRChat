77f979d273de743ba706458fac5887bc2fcaae5e
Minh Tran <trancongminh503@gmail.com>
2020-06-27 16:06:27 +0700

    AddJsFile

---
+++ SignalRChat.Clients.Blazor/wwwroot/js/myscript.js
@@ -0,0 +1,6 @@
+﻿function activeNavClient(clientId) {
+	for (var item of document.getElementsByClassName("list-group")[0].children) {
+		item.classList.remove("active");
+	}
+	document.getElementById(clientId).classList.add("active");
+}
